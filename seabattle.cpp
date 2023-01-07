7f0f53f797932f25e29606a6fd3edbf584bc9763
Vitaliy Polyakov <74113563+vspolyakov@users.noreply.github.com>
2023-01-07 14:13:07 +0300

    Console game Sea Battle

---
+++ seabattle.cpp
@@ -0,0 +1,13 @@
+/*
+    Консольная игра для двух игроков "Классический морской бой".
+    Vitaliy Polyakov (c), polyakov_vs@icloud.com, 2023.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
