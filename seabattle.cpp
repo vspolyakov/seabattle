35923c9d5a07c363e0c6400f20cdcedba74e70e7
Vitaliy Polyakov <polyakov.vs@mail.ru>
2026-01-15 14:11:34 +0300

    Add main seabattle.cpp file

---
+++ seabattle.cpp
@@ -0,0 +1,12 @@
+// seabattle.cpp
+// Development of the console game "Sea Battle" in order to learn the 
+// C++ programming language.
+//
+// Vitaliy Polyakov (C), polyakov.vs@mail.ru, 2026.
+
+#include <iostream>
+
+int main()
+{
+
+} // end main() function
